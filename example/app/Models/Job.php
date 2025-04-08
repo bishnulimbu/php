bbf4f19020a36a08545d5b083a2d897b17f1646e
bishnulimbu <subashlimbu987654321@gmail.com>
2025-04-08 18:31:37 +0545

    Fix table name in Job model and add relationships to Employer and Tag models

--- example/app/Models/Job.php
+++ example/app/Models/Job.php
@@ -8,8 +8,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Job extends Model{
 use HasFactory;
-    protected $table = 'job_listings';
+    protected $table = 'job_listing';
     protected $fillable= ['title', 'salary'];
+
+
+    public function employer(){
+        return $this->belongsTo(Employer::class);
     }
+    public function tags(){
+        return $this->belongsToMany(Tag::class,foreignPivotKey:'job_listing_id');
+    }
+    }
+
 
 
