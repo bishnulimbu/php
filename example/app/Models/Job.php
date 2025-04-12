3b14c5e311a8cc979cc93eb4bea9bf21e5040ff4
bishnulimbu <subashlimbu987654321@gmail.com>
2025-04-12 15:14:20 +0545

    feat(jobs): Update Job model to use guarded property and define relations

--- example/app/Models/Job.php
+++ example/app/Models/Job.php
@@ -6,19 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
-class Job extends Model{
-use HasFactory;
+class Job extends Model
+{
+    use HasFactory;
     protected $table = 'job_listing';
-    protected $fillable= ['title', 'salary'];
+    // protected $fillable = ['title', 'salary', 'employer_id'];
+    protected $guarded = []; //will disable fillable constrant
 
 
-    public function employer(){
+    public function employer()
+    {
         return $this->belongsTo(Employer::class);
     }
-    public function tags(){
-        return $this->belongsToMany(Tag::class,foreignPivotKey:'job_listing_id');
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, foreignPivotKey: 'job_listing_id');
     }
-    }
-
-
-
+}
