eeab62771410656110a76c615e1a36910e8eb111
bishnulimbu <subashlimbu987654321@gmail.com>
2025-04-17 17:27:36 +0545

    register logic for users

--- example/app/Http/Controllers/RegisteredUserController.php
+++ example/app/Http/Controllers/RegisteredUserController.php
@@ -2,17 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
 
 class RegisteredUserController extends Controller
 {
     //
     public function create()
     {
-        return view('auth.login');
+        return view('auth.register');
     }
     public function store()
     {
-        dd(request()->all());
+        // dd(request()->all());
+        // validate->create->logion
+        request()->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'email' => ['required', 'email'],
+            'password' => ['required', 'confirmed',Password::min(6)],
+        ]);
+       $user= User::create([
+            'first_name'=>request('first_name'),
+            'last_name'=>request('last_name'),
+            'email'=>request('email'),
+            'password'=>Hash::make(request('password')),
+        ]);
+        
+        Auth::login($user);
+        return redirect('/')->with('success', 'Logged Out successfully!');
     }
 }
