ca5761eb6681811d85d32deab41a247824b26b92
bishnulimbu <subashlimbu987654321@gmail.com>
2025-04-17 17:27:49 +0545

    login and session handling logic for user

--- example/app/Http/Controllers/SessionController.php
+++ example/app/Http/Controllers/SessionController.php
@@ -3,15 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
     public function create()
     {
-        return view('auth.register');
+        return view('auth.login');
     }
     public function store()
     {
-        dd(request()->all());
+        $attributes=request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+        if(!Auth::attempt($attributes)){
+            throw ValidationException::withMessages([
+                'email'=>"Invalid Email Credentials",//will only need one error message for security reasons
+            ]);
+        }
+        
+        request()->session()->regenerate();//regenerate session for security reasons
+        return redirect('/')->with('success','Logged In successfully!');
+    }
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect('/')->with('success', 'Logged Out successfully!');
     }
 }
