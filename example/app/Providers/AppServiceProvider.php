a452af779747e4bfb640d0f876df1c0f17bdb6a7
bishnulimbu <subashlimbu987654321@gmail.com>
2025-04-17 17:29:55 +0545

    added but commented gate logics for global reach and suchs

--- example/app/Providers/AppServiceProvider.php
+++ example/app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Models\Job;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +28,10 @@ class AppServiceProvider extends ServiceProvider
         // Model::preventLazyLoading();
         //can be done here for preventLazyLoading as the begining but will give error if lazy loading is done.
         Paginator::useTailwind();
+
+        // Gate defination for global reach
+    //     Gate::define('edit-job',function (User $user,Job $job){
+    //    return $job->employer->user->is($user);
+    //     });replaced with policy
     }
 }
