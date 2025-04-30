b094fb4a2400b255dba0047e700d331b9e36d43c
bishnulimbu <subashlimbu987654321@gmail.com>
2025-04-30 18:57:56 +0545

    custom navbar with logic integrated

--- Form1/resources/views/components/navbar.blade.php
+++ Form1/resources/views/components/navbar.blade.php
@@ -1,23 +1,40 @@
 <nav class="bg-gray-100 py-4">
     <div class="container mx-auto flex justify-between items-center">
         <div>
-            <a href="{{ route('/') }}" class="text-blue-500 hover:text-blue-700 font-semibold">Home</a>
+            <a href="{{ route('home') }}" class="text-blue-500 hover:text-blue-700 font-semibold">Home</a>
         </div>
         <div class="flex space-x-6">
             <a href="{{ route('forms.index') }}" class="text-blue-500 hover:text-blue-700 font-semibold">Form</a>
             <a href="{{ route('todos.index') }}" class="text-blue-500 hover:text-blue-700 font-semibold">Todo</a>
+            <a href="{{ route('posts.index') }}" class="text-blue-500 hover:text-blue-700 font-semibold">Post</a>
         </div>
         <div>
-            @if (Route::currentRouteName() == 'forms.index')
-                <a href="{{ route('forms.create') }}"
-                    class="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-md transition-colors duration-150">Create
-                    Form</a>
-            @endif
-            @if (Route::currentRouteName() == 'todos.index')
-                <a href="{{ route('todos.create') }}"
-                    class="bg-green-500 hover:bg-green-700 text-white font-semibold py-2 px-4 rounded-md transition-colors duration-150">Create
-                    Todo</a>
-            @endif
+                <x-createRouteNav routeName="forms">Create Form</x-createRouteNav>
+                <x-createRouteNav routeName="todos">Create Todo</x-createRouteNav>
+                <x-createRouteNav routeName="posts">Create Post</x-createRouteNav>
         </div>
+<div class="relative" x-data="{ open: false }">
+    <button @click="open = !open" class="text-blue-500 hover:text-blue-700 font-semibold">
+        @auth
+            {{ Auth::user()->name }}
+        @else
+            Account
+        @endauth
+    </button>
+
+    <div x-show="open" @click.away="open = false" class="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg">
+        @auth
+            <div class="px-4 py-2 text-gray-700">{{ Auth::user()->name }}</div>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <button type="submit" class="block w-full text-left px-4 py-2 text-blue-500 hover:bg-gray-100">Logout</button>
+            </form>
+        @else
+            <a href="{{ route('login') }}" class="block px-4 py-2 text-blue-500 hover:bg-gray-100">Login</a>
+            <a href="{{ route('register') }}" class="block px-4 py-2 text-blue-500 hover:bg-gray-100">Register</a>
+        @endauth
     </div>
-</nav>
+</div>
+</div>
+    </div> <!-- This closes the container div -->
+</nav> <!-- This closes the nav tag -->
