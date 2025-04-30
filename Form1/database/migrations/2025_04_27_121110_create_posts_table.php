a9100d612887642400db9f760dcfd984f72cc3a1
bishnulimbu <subashlimbu987654321@gmail.com>
2025-04-30 18:57:19 +0545

    feat: Add database migrations for posts, categories, and comments

---
+++ Form1/database/migrations/2025_04_27_121110_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->foreignIdFor(Category::class)->constrained()->onDelete('cascade');
+            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->text('body');
+            $table->boolean('is_published')->default(0);
+            $table->softDeletes();
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
