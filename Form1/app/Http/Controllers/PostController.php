247a33bfbf189e76a1067c971afccb7acf3e05a4
bishnulimbu <subashlimbu987654321@gmail.com>
2025-05-08 21:30:42 +0545

    feat: Enhance PostController with user-specific post retrieval and image handling

--- Form1/app/Http/Controllers/PostController.php
+++ Form1/app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Image;
 use App\Models\Post;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
@@ -11,9 +12,31 @@ class PostController extends Controller
     /**
      * Display a listing of the resource.
      */
+    //will define 3 different methods for different users
+    private function getPosts(){
+        if
+        (auth()->check() && auth()->user()->is_admin){
+        return Post::with('images')->latest()->paginate(6);
+        }else{
+        return Post::with('images')
+                    ->where('user_id',auth()->id())
+                    ->latest()
+                    ->paginate(6);
+        }
+    } 
+
+    public function adminIndex(){
+        $posts= $this->getPosts();
+        return view('posts.admin',compact('posts'));
+    }
+
     public function index()
     {
-        $posts=Post::latest()->paginate(6);
+        // $posts=Post::with(['images'=>function($query){
+        //     $query->latest()->limit(3);
+        // }])->latest()->paginate(6);
+        // $posts= $this->getPosts();
+        $posts=Post::with('images')->where('is_published',true)->latest()->paginate(6);
         return view('posts.index',compact('posts'));
     }
 
@@ -38,21 +61,37 @@ class PostController extends Controller
             'category_id'=>'required|exists:categories,id',
             'body'=>'required|string|min:10',
             'is_published'=>'boolean',
-            'slug'=>'nullable|string|unique:posts,slug'
+            'slug'=>'nullable|string|unique:posts,slug',
+            'images'=>'required',
+            'images.*'=>'image|mimes:jpeg,png,jpg,svg,gif,webp|max:10000'
         ]);
         $validated['slug']=$validated['slug']??Str::slug($validated['title']);
         $validated['user_id']=auth()->id();
+
+        $post =Post::create($validated);
         
-        Post::create($validated);
+        if($request->hasFile('images')){
+            foreach($request->file('images') as $imageFile){
+                 $fileName = Str::random(20) . '_' . $imageFile->getClientOriginalName();
+                
+                // Store the file
+                $filePath = $imageFile->storeAs('posts/' . $post->id, $fileName, 'public');
+                Image::create([
+                    'post_id'=>$post->id,
+                    'image_path'=>$filePath
+                ]);
+            }
+        }
+        
         return redirect()->route('posts.index')->with('success','post added successfully');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
-        //
+        return view('posts.show',compact('post'));
     }
 
     /**
@@ -74,8 +113,9 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('admin')->with('success','Post deleted successfully');
     }
 }
