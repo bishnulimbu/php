253ae8806e9490b4303c543e9d25ab0f45c02722
bishnulimbu <subashlimbu987654321@gmail.com>
2025-05-08 21:30:57 +0545

    feat: Add images relationship to Post model

--- Form1/app/Models/Post.php
+++ Form1/app/Models/Post.php
@@ -19,4 +19,7 @@ class Post extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    public function images(){
+        return $this->hasMany(Image::class);
+    }
 }
