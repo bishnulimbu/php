811052295ad12e156911b2e0fecf4891460d7733
bishnulimbu <subashlimbu987654321@gmail.com>
2025-05-16 11:17:51 +0545

    feat: Implement ImageController with CRUD operations for image management

---
+++ Form1/app/Http/Controllers/ImageController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Image;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Image $image)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Image $image)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Image $image)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Image $image)
+    {
+
+        try{
+        Storage::delete('public/'.$image->image_path);            
+        $image->delete();
+        return response()->json([
+            'success'=>'Image deleted successfully',
+        ]);
+        }catch(\Exception $e){
+            return response()->json([
+                'message'=>'Image not deleted'
+            ]);
+        }
+    }
+}
